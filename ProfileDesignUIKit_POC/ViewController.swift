96705058de2c26c8cc6bd5fd692c336a3ac38863
Julien Avalle <julien.avalle@gmail.com>
2022-01-07 11:36:38 +0100

    First Commit

---
+++ ProfileDesignUIKit_POC/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ProfileDesignUIKit_POC
+//
+//  Created by Kirito04800 on 07/01/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ README.md
@@ -0,0 +1,1 @@
+# ProfileDesignUIKit_POC
